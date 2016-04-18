a1c3d42c7bd29917d5edcc32a1114795928e3d99
Mariano Custiel <mcustiel@goodgamestudios.com>
2016-04-18 12:20:27 +0200

    Revert "Updated october to latest"
    This reverts commit 393b2ed9e728ca9799e36f67b6656383a0f9d8ae.

--- modules/cms/classes/Controller.php
+++ modules/cms/classes/Controller.php
@@ -753,7 +753,6 @@ class Controller
     public function renderPartial($name, $parameters = [], $throwException = true)
     {
         $vars = $this->vars;
-        $this->vars = array_merge($this->vars, $parameters);
 
         /*
          * Alias @ symbol for ::
